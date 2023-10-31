0a865613b6f76cfcd03438784889c198d12f2059
Jason Ky <744169@student.dusd.net>
2023-10-31 15:28:20 -0700

    i done controls and tilemaps
    i finished the controls, stilll need to make the water floor tillemaps.

--- RubysAdventureKyP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureKyP7/Assets/Scripts/RubyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class RubyController : MonoBehaviour
@@ -7,14 +8,18 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertcal = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + 1f;
+        position.x = position.x  + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y  + 5.0f * vertcal * Time.deltaTime;
         transform.position = position;
     }
 }
