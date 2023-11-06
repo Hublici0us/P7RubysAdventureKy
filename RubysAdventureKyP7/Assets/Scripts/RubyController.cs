b0322df5653a02b44889e31a585a4ccc220e813b
Jason Ky <744169@student.dusd.net>
2023-11-06 15:27:06 -0800

    finished decorating, doing scripts for rigidbody
    organized decorations, making rigidbody2d and boxcollider2d scripts

--- RubysAdventureKyP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureKyP7/Assets/Scripts/RubyController.cs
@@ -2,24 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertcal = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
+    void FixedUpdate()
+    {
         Vector2 position = transform.position;
-        position.x = position.x  + 5.0f * horizontal * Time.deltaTime;
-        position.y = position.y  + 5.0f * vertcal * Time.deltaTime;
-        transform.position = position;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
+
+        rigidbody2d.position = position;
     }
 }
