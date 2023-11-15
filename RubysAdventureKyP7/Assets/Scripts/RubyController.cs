80f78bfda7d6fdc743f171803f262f0272082ff1
Jason Ky <744169@student.dusd.net>
2023-11-14 22:46:04 -0800

    Created HealthCollectible Script
    I created a functioning Health Collectible. == is equal to, != is not equal to.

--- RubysAdventureKyP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureKyP7/Assets/Scripts/RubyController.cs
@@ -19,7 +19,7 @@ public class RubyController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
 
-        currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -38,7 +38,7 @@ public class RubyController : MonoBehaviour
         rigidbody2d.position = position;
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
