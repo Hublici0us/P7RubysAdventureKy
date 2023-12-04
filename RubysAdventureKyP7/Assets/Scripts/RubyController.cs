a3c693769da3059c018bd499d456b450fbf1a31c
Jason Ky <744169@student.dusd.net>
2023-12-04 10:00:32 -0800

    finshed build
    i created the builld folder, then now I can send the build to friends. Need to record.

--- RubysAdventureKyP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureKyP7/Assets/Scripts/RubyController.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
 {
